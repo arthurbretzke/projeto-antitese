afb47b74e8328ce0b0bdc9ac65a61e4d846fba02
Arthur <arthur.bretzke@gmail.com>
2025-10-04 20:43:16 -0300

    Construção do ambiente.

---
+++ antitese-financeiro/src/test/java/br/com/arthur/antitese_financeiro/AntiteseFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arthur.antitese_financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiteseFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
